202213ca295c25e1a4e2b0438f787349a98557ed
leandro-gft <lpsacchi@gmail.com>
2020-05-05 15:02:07 -0300

    Adicionada busca do post por id

---
+++ src/main/java/br/com/gft/workshopmongo/service/PostService.java
@@ -0,0 +1,20 @@
+package br.com.gft.workshopmongo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gft.workshopmongo.domain.Post;
+import br.com.gft.workshopmongo.repository.PostRepository;
+import br.com.gft.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+
+}
