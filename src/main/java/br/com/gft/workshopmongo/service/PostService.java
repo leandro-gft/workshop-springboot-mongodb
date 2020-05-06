acbd79408d5452a9ffc0d1f8585ac11c48c27d30
leandro-gft <lpsacchi@gmail.com>
2020-05-06 11:54:17 -0300

    Consulta com vários query

--- src/main/java/br/com/gft/workshopmongo/service/PostService.java
+++ src/main/java/br/com/gft/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package br.com.gft.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return postRepository.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 
 }
